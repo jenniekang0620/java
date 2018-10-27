b3f2242454848a9760972143caa859dcaf2e1b4d
Jennie.Kang <jennie.kang0620@gmail.com>
2018-10-28 05:09:44 +0900

    week7

---
+++ Doublesided2.java
@@ -0,0 +1,13 @@
+
+public class Doublesided2 {
+
+    public static void main(String[] args) {
+
+       double num = Math.random();
+       if (num<.5) {
+           System.out.print("HEADS");
+       } else {
+           System.out.print("TAILS");
+       }
+    }
+}
